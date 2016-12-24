d0aaaf8b67212dd0ede51797fd7dcd83babb1d3f
dutengfei <1432308953@qq.com>
2016-12-24 20:44:39 +0800

    changs one File

---
+++ MenusController.java
@@ -0,0 +1,26 @@
+package com.aaa.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MenusController {
+
+	@RequestMapping("asdfghjkerty")
+	public String save(HttpServletRequest reuqest){
+		System.out.println("--------save------");
+		reuqest.setAttribute("name", "zhangsan");
+		return "index.jsp";
+	}
+	@RequestMapping("save1")
+	public ModelAndView save1(){
+		System.out.println("--------save------");
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index.jsp");
+		mv.addObject("name", "lisi");
+		return mv;
+	}
+}
